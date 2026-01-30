59267582806a192796a329450d14def1b4ff1e0e
Jirayut Najan <tonklaeiei123@gmail.com>
2026-01-30 21:51:21 +0700

    transparent

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -4,12 +4,12 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    -- opts = {
-    --   transparent = true, -- เปิดความโปร่งใสที่นี่
-    --   styles = {
-    --     sidebars = "transparent", -- ทำให้แถบข้าง (Explorer) ใส
-    --     floats = "transparent",   -- ทำให้หน้าต่างลอย (Floating windows) ใส
-    --   },
-    -- },
+    opts = {
+      transparent = true, -- เปิดความโปร่งใสที่นี่
+      -- styles = {
+      --   sidebars = "transparent", -- ทำให้แถบข้าง (Explorer) ใส
+      --   floats = "transparent",   -- ทำให้หน้าต่างลอย (Floating windows) ใส
+      -- },
+    },
   },
 }
