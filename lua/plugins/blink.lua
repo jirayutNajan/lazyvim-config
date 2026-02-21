3379d6aed9df2fa1f5e23812047f587ee48982bf
Jirayut Najan <tonklaeiei123@gmail.com>
2026-02-21 17:26:41 +0700

    disable blink text

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,13 @@
+-- lua/plugins/blink.lua (หรือไฟล์ config เดิมของคุณ)
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        ghost_text = {
+          enabled = false,
+        },
+      },
+    },
+  },
+}
